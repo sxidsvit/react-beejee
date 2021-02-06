d2fedb8665b11ab094a0782fce6ce0d919c02657
sxidsvit <s.p.antonyuk@gmail.com>
2021-02-07 01:10:28 +0200

    const cors

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,11 @@
 // const admin = 'admin'
 // const password = 123
 const developer = 'sxidsvit'
+const cors = 'https://cors-anywhere.herokuapp.com/'
 export const dataPerPage = 3
 
 export const baseUrl = 'https://uxcandy.com/~shapoval/test-task-backend/v2/'
 
-export const getUrl = `${baseUrl}?developer=${developer}`
+export const getUrl = `${cors}${baseUrl}?developer=${developer}`
 
-export const createUrl = `${baseUrl}create?developer=${developer}`
+export const createUrl = `${cors}${baseUrl}create?developer=${developer}`
